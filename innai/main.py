f61a9504c8339dd165dec76bc0d9109d79c6012c
Jakob <mail@bauer-jakob.de>
2023-12-29 10:31:38 +0100

    Played with machine learning parameters

--- innai/main.py
+++ innai/main.py
@@ -1,14 +1,17 @@
+import torch
 from torch.utils.data import DataLoader
 
 from data_set.innai_data_set import InnAiDataSet
 
-dataset = InnAiDataSet()
-dataloader = DataLoader(dataset=dataset, batch_size=4, shuffle=True)
-
-for batch_idx, (precipitationMaps, currentInnLevels, nextInnLevels) in enumerate(dataloader):
-    print(precipitationMaps.size())
+# dataset = InnAiDataSet()
+# dataloader = DataLoader(dataset=dataset, batch_size=4, shuffle=True)
+#
+# for batch_idx, (precipitationMaps, currentInnLevels, nextInnLevels) in enumerate(dataloader):
+#     print(precipitationMaps.size())
 
 
 # print(len(dataset))
 # image, levels, prediction = dataset.__getitem__(5)
+
+model = torch.load('./checkpoints/model.pt')
 
--- innai/train.py
+++ innai/train.py
@@ -6,6 +6,7 @@ import torch.nn.functional as F
 from torch.autograd import Variable
 import torch.nn as nn
 import torch
+import matplotlib.pyplot as plt
 
 full_dataset = InnAiDataSet()
 
@@ -19,41 +20,13 @@ test_data_loader = DataLoader(test_dataset, batch_size=1)
 
 # dataloader = DataLoader(dataset, batch_size=5, shuffle=True)
 
-
-
 model = InnAiModel()
-optimizer = optim.Adam(model.parameters(), lr=0.001)
+optimizer = optim.Adam(model.parameters(), lr=0.00002)
 criterion = nn.MSELoss()
-epochs = 30
-epoch = 0
+epochs = 10
 
-def train(epoch):
-    global model, optimizer, criterion
-
-    model.train()
-
-    for batch_idx, (precipitationMaps, currentInnLevels, nextInnLevels) in enumerate(train_data_loader):
-        # precipitationData = Variable(precipitationMaps)
-        # currentInnLevelsData = Variable(currentInnLevels)
-
-        input = torch.cat((precipitationMaps, currentInnLevels), 1)
-        inputVariable = Variable(input)
-        optimizer.zero_grad()
-
-        out = model(inputVariable)
-
-        loss = criterion(out, nextInnLevels)
-
-        loss.backward()
-
-        optimizer.step()
-        print('Epoch [{}/{}], Loss: {:.4f}'.format(epoch, batch_idx * len(precipitationMaps), loss.data))
-
-        torch.save(model, "./checkpoints/innAiModel.pth")
-
-for epoch in range(epochs):
-    train(epoch)
-
+loss_values = []
+process_values = []
 
 def evaluate_model():
     global model, criterion
@@ -70,6 +43,46 @@ def evaluate_model():
         loss = criterion(out, nextInnLevels)
         losses.append(loss)
 
-    return sum(losses) / len(losses)
+    return (sum(losses) / len(losses)).item()
+
+def train(epoch):
+    global model, optimizer, criterion, loss_values
+
+    if epoch >= 30:
+        optimizer = optim.Adam(model.parameters(), lr=0.009)
+
+    model.train()
+
+    for batch_idx, (precipitationMaps, currentInnLevels, nextInnLevels) in enumerate(train_data_loader):
+        # precipitationData = Variable(precipitationMaps)
+        # currentInnLevelsData = Variable(currentInnLevels)
+
+        input = torch.cat((precipitationMaps, currentInnLevels), 1)
+        inputVariable = Variable(input)
+        optimizer.zero_grad()
+
+        out = model(inputVariable)
+
+        loss = criterion(out, nextInnLevels)
+        loss_values.append(loss.data)
+
+        loss.backward()
+
+        optimizer.step()
+        print('Epoch [{}/{}], Loss: {:.4f}'.format(epoch, batch_idx * len(precipitationMaps), loss.data))
+
+        torch.save(model.state_dict(), "./checkpoints/model.pt")
+
+    process_values.append(evaluate_model())
+
+for epoch in range(epochs):
+    train(epoch)
 
 print(evaluate_model())
+
+f1 = plt.figure(1)
+plt.plot(loss_values, 'b-')
+
+f2 = plt.figure(2)
+plt.plot(process_values, 'r-')
+plt.show()
